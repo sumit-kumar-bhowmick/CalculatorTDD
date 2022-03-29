cac567724bed4f3b5139caab934b0aacbcd94ab2
Sumit Bhowmick <sumit.k.bhowmick>
2022-03-29 11:17:36 +0530

    refactored the add function and added logic to pass the second test case.

--- calculator.py
+++ calculator.py
@@ -1,4 +1,13 @@
+import re
 
-def add(inputString:str):
-    if len(inputString) == 0:
+
+def add(input_string: str):
+
+    number_list = re.findall("(\d+)", input_string)  # 1 12
+
+    if len(number_list) == 0:
         return 0
+
+    number_list = [int(number) for number in number_list]
+
+    return sum(number_list)
