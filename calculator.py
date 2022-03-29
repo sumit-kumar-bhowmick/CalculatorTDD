6ea8ab70073ce7bce60b9f2671e56a8451031a2e
Sumit Bhowmick <sumit.k.bhowmick>
2022-03-29 11:36:41 +0530

    added the logic for "not allowed separator" test case.

--- calculator.py
+++ calculator.py
@@ -8,6 +8,9 @@ def add(input_string: str):
     if len(number_list) == 0:
         return 0
 
+    if (",\n" in input_string) or ("\n," in input_string):
+        return f'''wrong input,the input contain "\\n," or ",\\n".provide any one of them as separator.ie , or \\n'''
+
     number_list = [int(number) for number in number_list]
 
     return sum(number_list)
