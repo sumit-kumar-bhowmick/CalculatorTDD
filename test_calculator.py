b91da72859eadef5f82f5151e6279a39035f4988
Sumit Bhowmick <sumit.k.bhowmick>
2022-03-29 11:09:33 +0530

    added single number as input string test case.

--- test_calculator.py
+++ test_calculator.py
@@ -4,3 +4,10 @@ from calculator import add
 def test_add_empty_string():
     # test for empty string as the input
     assert add("") == 0
+
+
+def test_add_single_number():
+    # test for single number as input
+    assert add("1") == 1
+    assert add("12") == 12
+    assert add("123") == 123
