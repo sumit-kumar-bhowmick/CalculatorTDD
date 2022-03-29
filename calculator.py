e03b6378133ef7256547271018e4dc119793a623
Sumit Bhowmick <sumit.k.bhowmick>
2022-03-29 11:49:49 +0530

    added logic for not allowing negative number in input string.

--- calculator.py
+++ calculator.py
@@ -2,8 +2,7 @@ import re
 
 
 def add(input_string: str):
-
-    number_list = re.findall("(\d+)", input_string)  # 1 12
+    number_list = re.findall("(\d+|-\d+)", input_string)  # 1 12
 
     if len(number_list) == 0:
         return 0
@@ -12,5 +11,9 @@ def add(input_string: str):
         return f'''wrong input,the input contain "\\n," or ",\\n".provide any one of them as separator.ie , or \\n'''
 
     number_list = [int(number) for number in number_list]
+
+    negative_numbers = list(filter(lambda x: x < 0, number_list))
+    if len(negative_numbers) != 0:
+        raise ValueError(f"Negative number not allowed.given negative values are {negative_numbers}")
 
     return sum(number_list)
